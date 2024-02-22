3304700097154e9d2a863130b9ba743b3b12a08c
Chika Iwanaga <c.iwanaga@8.mpd.ac.jp>
2024-02-22 17:57:26 +0900

    xerograv_web DAY1_20240222

---
+++ tests/Feature/DivelogLikeTest.php
@@ -0,0 +1,40 @@
+// tests/Feature/DivelogLikeTest.php
+
+<?php
+
+use App\Models\User;
+use App\Models\Divelog;
+
+// likeのテスト
+it('allows a user to like a divelog', function () {
+  $user = User::factory()->create();
+  $divelog = Divelog::factory()->create();
+
+  $this->actingAs($user)
+    ->post(route('divelogs.like', ['divelog' => $divelog->id]))
+    ->assertStatus(302);
+
+  $this->assertDatabaseHas('divelog_user', [
+    'user_id' => $user->id,
+    'divelog_id' => $divelog->id
+  ]);
+});
+
+// dislikeのテスト
+it('allows a user to dislike a divelog', function () {
+  $user = User::factory()->create();
+  $divelog = Divelog::factory()->create();
+
+  // 最初にlikeをする
+  $user->likes()->attach($divelog);
+
+  $this->actingAs($user)
+    ->delete(route('divelogs.dislike', ['divelog' => $divelog->id]))
+    ->assertStatus(302);
+
+  $this->assertDatabaseMissing('divelog_user', [
+    'user_id' => $user->id,
+    'divelog_id' => $divelog->id
+  ]);
+});
+
